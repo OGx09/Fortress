8812c613c3c81d7d85ab64bf7e95ebf379f7bc2b
Gbenga Oladipupo <michaelgbenga91@gmail.com>
2021-05-24 02:20:35 +0100

    Added Database

--- app/src/main/java/com/example/myapplication/features/repository/database/FortressDatabase.kt
+++ app/src/main/java/com/example/myapplication/features/repository/database/FortressDatabase.kt
@@ -3,7 +3,7 @@ package com.example.myapplication.features.repository.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = arrayOf(PasswordEntity::class), version = 1)
+@Database(entities = [PasswordEntity::class], version = 1)
 abstract class FortressDatabase : RoomDatabase() {
     abstract fun passwordDao(): FortressDao
 }
