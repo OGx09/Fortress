c46fcd408dc0ca240edf6fd80d2e981e0b68f557
Gbenga Oladipupo <michaelgbenga91@gmail.com>
2021-08-07 15:32:16 +0100

    Cleaning up

--- app/src/main/java/com/example/myapplication/utils/EncryptionUtils.kt
+++ app/src/main/java/com/example/myapplication/utils/EncryptionUtils.kt
@@ -85,92 +85,6 @@ class EncryptionUtilsImpl @Inject constructor(private val dao: FortressDao) : En
             .build()
     }
 
-    fun encrypt(plainText: ByteArray, key: SecretKey, iv: ByteArray): ByteArray{
-        val _cipher = Cipher.getInstance("AES")
-        val secretKeySpec = SecretKeySpec(key.encoded, "AES")
-        val ivSpec = IvParameterSpec(iv)
-        _cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivSpec)
-        val cipherText = _cipher.doFinal(plainText)
-        return plainText
-    }
-
-    fun decrypt(key: SecretKey, cipherText: ByteArray, iv: ByteArray): String{
-        try {
-            val cipher = Cipher.getInstance("AES")
-            val keySpec = SecretKeySpec(key.encoded, "AES")
-            val ivSpec = IvParameterSpec(iv)
-            cipher.init(Cipher.DECRYPT_MODE, keySpec, ivSpec)
-            val decryptedText = cipher.doFinal(cipherText)
-            return String(decryptedText)
-        }catch (e: Exception){
-
-        }
-        return ""
-    }
-
-
-
-    @Throws(java.lang.Exception::class)
-    fun encrypt(message: String): String {
-        val md = MessageDigest.getInstance("SHA-1")
-        val digestOfPassword = md.digest(
-            token.toByteArray()
-        )
-        val keyBytes: ByteArray = Arrays.copyOf(digestOfPassword, 24)
-        var j = 0
-        var k = 16
-        while (j < 8) {
-            keyBytes[k++] = keyBytes[j++]
-        }
-        val key: SecretKey = SecretKeySpec(keyBytes, "DESede")
-        val s1 = "12345678"
-        val bytes = s1.toByteArray()
-        val iv = IvParameterSpec(bytes)
-        val cipher = Cipher.getInstance("DESede/CBC/PKCS5Padding")
-        cipher.init(Cipher.ENCRYPT_MODE, key, iv)
-        val plainTextBytes = message.toByteArray(charset("utf-8"))
-        val cipherText = cipher.doFinal(plainTextBytes)
-        val ss: String = base64encoding(cipherText, 0)
-        val result: ByteArray = base64decodingByte(cipherText, 0)
-        return String(result, StandardCharsets.UTF_8)
-    }
-
-    fun base64decodingByte(context: ByteArray?, type: Int): ByteArray {
-        val result: ByteArray = Base64.encode(context, type)
-        return result
-    }
-
-    private fun base64encoding(context: ByteArray?, type: Int): String {
-        var result: String? = ""
-        result = Base64.encodeToString(context, type)
-        return result
-    }
-
-    @Throws(java.lang.Exception::class)
-    fun decrypt(message: ByteArray?): String {
-        val values: ByteArray = base64decodingByte(message, 0)
-        val md = MessageDigest.getInstance("SHA-1")
-        val digestOfPassword = md.digest(
-            token
-                .toByteArray(Charset.defaultCharset())
-        )
-        val keyBytes: ByteArray = Arrays.copyOf(digestOfPassword, 24)
-        var j = 0
-        var k = 16
-        while (j < 8) {
-            keyBytes[k++] = keyBytes[j++]
-        }
-        val key: SecretKey = SecretKeySpec(keyBytes, "DESede")
-        val s1 = "12345678"
-        val bytes = s1.toByteArray()
-        val iv = IvParameterSpec(bytes)
-        val decipher = Cipher.getInstance("DESede/CBC/PKCS5Padding")
-        decipher.init(Cipher.DECRYPT_MODE, key, iv)
-        val plainText = decipher.doFinal(values)
-        return String(plainText, StandardCharsets.UTF_8)
-    }
-
-
 
     override suspend fun encryptSecretInformation(cipher: Cipher, passwordEntity: PasswordEntity) {
         // Exceptions are unhandled for getCipher() and getSecretKey().
@@ -223,26 +137,6 @@ class EncryptionUtilsImpl @Inject constructor(private val dao: FortressDao) : En
 
 
 
-    private fun decryptSecretInformation(data: String) {
-        // Exceptions are unhandled for getCipher() and getSecretKey().
-        val cipher = getCipher()
-        val secretKey = getSecretKey()
-        try {
-            cipher.init(Cipher.DECRYPT_MODE, secretKey)
-            val encryptedInfo: ByteArray = cipher.doFinal(
-                "Hello world of django".toByteArray(Charset.defaultCharset())
-            )
-            Log.d(
-                "MY_APP_TAG212", "Encrypted information: " +
-                        Arrays.toString(encryptedInfo)
-            )
-        } catch (e: InvalidKeyException) {
-            Log.e("MY_APP_TAG", "Key is invalid.")
-        } catch (e: UserNotAuthenticatedException) {
-            Log.d("MY_APP_TAG", "The key's validity timed out.")
-           // biometricPrompt.authenticate(promptInfo)
-        }
-    }
 
 }
 
